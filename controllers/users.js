0bc1400b3dadee19b3a814a34e83a31c7eb4b27e
Manoj <manojkarthi68@gmail.com>
2021-09-19 06:53:36 -0300

    signup and login done.

--- controllers/users.js
+++ controllers/users.js
@@ -1,58 +1,53 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../models/user");
+const jwtSecretKey = "ranomize_this_later_@_blah_flah";
 
 
 router.post("/signup", (req, res, next) => {
-    console.log(req.body);
-    bcrypt.hash(req.body.password, 10).then(hash => {
+    bcrypt.hash(req.body.password, saltRounds).then(hash => {
         const user = new User({
             email: req.body.email,
             password: hash
         });
-        user.save()
-            .then(result => {
-                res.status(201).json({
-                    message: 'User Created',
-                    result: result
-                })
+        user.save().then(result => {
+            res.status(201).json({
+                message: "User Created",
             })
-            .catch(err=> {
-                res.status(500).json({
-                    error: err
-                });
-            })
+        })
+        .catch(err=> {
+            res.status(500).json({
+                error: err
+            });
+        })
     });
 });
 
-router.post("/login", (req, res, next) => {
-    User.findOne({email: req.body.email})
-    .then(user => {
+router.post("/login", (req, res) => {
+    User.findOne({email: req.body.email}).then(user => {
         if(!user) {
             return res.status(401).json({
-                message: "Auth failed"
+                message: "Email not registered."
             });
         }
-        return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result =>{
-        if(!result) {
-            return res.status(401).json({
-                message: "Auth failed"
-            });            
-        }
-        const token = jwt.sign({email: user.email, userid: user._id}, 'secret_this_should_be_longer', 
-        {expiresIn: '1h'}
-        );
-        res.status(200).json({
-            token: token
+        return bcrypt.compare(req.body.password, user.password).then(result => {
+            if(!result) {
+                return res.status(401).json({
+                    message: "Incorrect Password/email."
+                });                 
+            }
+            const token = jwt.sign({email: user.email, userid: user._id}, jwtSecretKey);
+            return res.status(200).json({
+                token: token
+            });
         });
-    })
-    .catch(err => {
+    }).catch(err => {
         return res.status(401).json({
-            message: "Auth failed"
+            message: "Failed to verify auth and generate session.",
+            error: err
         });
     });
 });
